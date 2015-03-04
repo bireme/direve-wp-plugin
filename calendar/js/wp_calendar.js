eac53510e155e6dc6719dbf5896bdc92dc498a1b
Wilson Moura <wilsonsmoura@gmail.com>
2015-03-04 17:08:55 -0300

    Layout adjustments in the events calendar.

--- calendar/js/wp_calendar.js
+++ calendar/js/wp_calendar.js
@@ -41,26 +41,26 @@ function monthFollowLink(month, year) {
 	jQuery('.ui-datepicker-month').after("&nbsp;");
 }
 
-jQuery(document).ready(function(){
-    jQuery('#wp-calendar').datepicker();
-    jQuery('#wp-calendar a.ui-state-active').addClass('wp-calendar-current-date').parent('td').addClass('wp-calendar-today-active');
+jQuery(document).ready(function($){
+    $('#wp-calendar').datepicker();
+    $('#wp-calendar a.ui-state-active').addClass('wp-calendar-current-date').parent('td').addClass('wp-calendar-today-active');
     
     function WP_Cal_get_posts_by_month(month, year){
         var check_date = month+'-'+year;
-        var found_at = jQuery.inArray( check_date , wp_cal_dates );
+        var found_at = $.inArray( check_date , wp_cal_dates );
         if( found_at == -1 ){
             
-            //jQuery(".wp_calendar .calendar_wrap_loading").removeClass('calendar_wrap_loading_hide').addClass('calendar_wrap_loading_show');
-            jQuery(".wp_calendar .circle_loading").removeClass('hide_circle').addClass('show_circle');
-            jQuery('#wp-calendar .ui-datepicker-prev').toggle();
-            jQuery('#wp-calendar .ui-datepicker-next').toggle();
-            jQuery('#calendar_wrap .calendar-pagi a').each(function(){
-                jQuery(this).removeClass('pagi_state_show').toggle();
+            //$(".wp_calendar .calendar_wrap_loading").removeClass('calendar_wrap_loading_hide').addClass('calendar_wrap_loading_show');
+            $(".wp_calendar .circle_loading").removeClass('hide_circle').addClass('show_circle');
+            $('#wp-calendar .ui-datepicker-prev').toggle();
+            $('#wp-calendar .ui-datepicker-next').toggle();
+            $('#calendar_wrap .calendar-pagi a').each(function(){
+                $(this).removeClass('pagi_state_show').toggle();
             });
-            jQuery('a.ui-state-default').removeAttr('href').css('text-decoration', 'none');
+            $('a.ui-state-default').removeAttr('href').css('text-decoration', 'none');
             
             wp_cal_dates.push(check_date);
-            jQuery.ajax({
+            $.ajax({
                 type    :   'POST',
                 url     :   wpCalendarObj.ajaxurl,
                 data    :   {
@@ -70,39 +70,39 @@ jQuery(document).ready(function(){
                     year            :   year
                 },
                 success :   function( data ){
-                    data = jQuery.parseJSON( data );
+                    data = $.parseJSON( data );
                     var posts = data.posts;
                     var this_dates = data.classes;
                     var curr_date = new Array();
                     var d = '';
-                    jQuery.each( this_dates, function (key , value){
+                    $.each( this_dates, function (key , value){
                         curr_date = key.split('-');
                         d = curr_date[2];
-                        var element = jQuery('#wp-calendar a.ui-state-default:contains("'+d+'")');
+                        var element = $('#wp-calendar a.ui-state-default:contains("'+d+'")');
                         if( element.length > 1 ){
-                            jQuery(element).each( function(){
-                                if( jQuery( this ).text() == d ){
-                                    jQuery( this ).parent('td').addClass('WP-Cal-popup');
+                            $(element).each( function(){
+                                if( $( this ).text() == d ){
+                                    $( this ).parent('td').addClass('WP-Cal-popup');
                                 }
                             });
                             
                         }
                         else{
-                            jQuery( element ).parent('td').addClass('WP-Cal-popup');
+                            $( element ).parent('td').addClass('WP-Cal-popup');
                         }
                     });
-                    jQuery('#wp-calendar .WP-Cal-popup').each( function (){
-                        var year = jQuery(this).attr('data-year');
-                        var month = jQuery(this).attr('data-month');
+                    $('#wp-calendar .WP-Cal-popup').each( function (){
+                        var year = $(this).attr('data-year');
+                        var month = $(this).attr('data-month');
                         month = parseInt( month ) + 1;
-                        var day = jQuery(this).find('a.ui-state-default').text();
+                        var day = $(this).find('a.ui-state-default').text();
                         var this_date = year + '-' + month + '-' + day;
                         var innerContent = posts[this_date];
                         if (/undefined/.test(innerContent)) {
-                            jQuery(this).removeClass('WP-Cal-popup');
+                            $(this).removeClass('WP-Cal-popup');
                         } else {
-                            jQuery(this).append( '<div class="wp-cal-tooltip">'+innerContent+'</div>' );
-                            jQuery(this).find('a.ui-state-default').attr('href', '?q=start_date:"'+year+'-'+month+'-'+day+'T00:00:00Z"');
+                            $(this).append( '<div class="wp-cal-tooltip">'+innerContent+'</div>' );
+                            $(this).find('a.ui-state-default').attr('href', '?q=start_date:"'+year+'-'+month+'-'+day+'T00:00:00Z"');
                         }
                     });
                     var new_data_element = {
@@ -113,13 +113,13 @@ jQuery(document).ready(function(){
                     wp_cal_posts.push( new_data_element );
                 },
                 complete: function() {
-                    jQuery('.ui-datepicker-next, .ui-datepicker-prev, .wp-cal-prev, .wp-cal-next').bind('click');
-                    //jQuery(".wp_calendar .calendar_wrap_loading").removeClass('calendar_wrap_loading_show').addClass('calendar_wrap_loading_hide');
-                    jQuery(".wp_calendar .circle_loading").removeClass('show_circle').addClass('hide_circle');
-                    jQuery('#wp-calendar .ui-datepicker-prev').toggle();
-                    jQuery('#wp-calendar .ui-datepicker-next').toggle();
-                    jQuery('#calendar_wrap .calendar-pagi a').each(function(){
-                        jQuery(this).removeClass("pagi_state_hide").toggle();
+                    $('.ui-datepicker-next, .ui-datepicker-prev, .wp-cal-prev, .wp-cal-next').bind('click');
+                    //$(".wp_calendar .calendar_wrap_loading").removeClass('calendar_wrap_loading_show').addClass('calendar_wrap_loading_hide');
+                    $(".wp_calendar .circle_loading").removeClass('show_circle').addClass('hide_circle');
+                    $('#wp-calendar .ui-datepicker-prev').toggle();
+                    $('#wp-calendar .ui-datepicker-next').toggle();
+                    $('#calendar_wrap .calendar-pagi a').each(function(){
+                        $(this).removeClass("pagi_state_hide").toggle();
                     });
                 }
             });
@@ -128,55 +128,55 @@ jQuery(document).ready(function(){
             var this_dates = {};
             var this_posts = {};
             
-            jQuery( wp_cal_posts ).each( function (){
+            $( wp_cal_posts ).each( function (){
                 if( this.month == check_date ){
                     this_dates = this.classes;
                     this_posts = this.posts;
                     var curr_date = new Array();
                     var d = '';
-                    jQuery.each( this_dates, function (key , value){
+                    $.each( this_dates, function (key , value){
                         curr_date = key.split('-');
                         d = curr_date[2];
-                        var element = jQuery('#wp-calendar a.ui-state-default:contains("'+d+'")');
+                        var element = $('#wp-calendar a.ui-state-default:contains("'+d+'")');
                         if( element.length > 1 ){
-                            jQuery(element).each( function(){
-                                if( jQuery( this ).text() == d ){
-                                    jQuery( this ).parent('td').addClass('WP-Cal-popup');
+                            $(element).each( function(){
+                                if( $( this ).text() == d ){
+                                    $( this ).parent('td').addClass('WP-Cal-popup');
                                 }
                             });
 
                         }
                         else{
-                            jQuery( element ).parent('td').addClass('WP-Cal-popup');
+                            $( element ).parent('td').addClass('WP-Cal-popup');
                         }
                     });
-                    jQuery('#wp-calendar .WP-Cal-popup').each( function (){
-                        var year = jQuery(this).attr('data-year');
-                        var month = jQuery(this).attr('data-month');
+                    $('#wp-calendar .WP-Cal-popup').each( function (){
+                        var year = $(this).attr('data-year');
+                        var month = $(this).attr('data-month');
                         month = parseInt( month ) + 1;
-                        var day = jQuery(this).find('a.ui-state-default').text();
+                        var day = $(this).find('a.ui-state-default').text();
                         var this_date = year + '-' + month + '-' + day;
                         var innerContent = this_posts[this_date];
                         if (/undefined/.test(innerContent)) {
-                            jQuery(this).removeClass('WP-Cal-popup');
+                            $(this).removeClass('WP-Cal-popup');
                         } else {
-                            jQuery(this).append( '<div class="wp-cal-tooltip">'+innerContent+'</div>' );
-                            jQuery(this).find('a.ui-state-default').attr('href', '?q=start_date:"'+year+'-'+month+'-'+day+'T00:00:00Z"');
+                            $(this).append( '<div class="wp-cal-tooltip">'+innerContent+'</div>' );
+                            $(this).find('a.ui-state-default').attr('href', '?q=start_date:"'+year+'-'+month+'-'+day+'T00:00:00Z"');
                         }
                     });
                 }
             });
         }
-        jQuery('a.ui-state-default').click( function (e){
+        $('a.ui-state-default').click( function (e){
             e.stopPropagation();
         });
     }
 
-    jQuery('.ui-datepicker-next, .ui-datepicker-prev, .wp-cal-prev, .wp-cal-next').live('click', function (){
-        var Month = jQuery('#wp-calendar .ui-datepicker-month #monthnum').val();
-        var Year = jQuery('#wp-calendar .ui-datepicker-year').text();
+    $('.ui-datepicker-next, .ui-datepicker-prev, .wp-cal-prev, .wp-cal-next').live('click', function (){
+        var Month = $('#wp-calendar .ui-datepicker-month #monthnum').val();
+        var Year = $('#wp-calendar .ui-datepicker-year').text();
         Month = WP_Cal_convertMonth(Month);
-        jQuery('a.query-month').attr('href', '?q=start_date:[' + Year + '-' + Month + '-01T00:00:00Z+TO+' + Year + '-' + Month + '-31T00:00:00Z]');
+        $('a.query-month').attr('href', '?q=start_date:[' + Year + '-' + Month + '-01T00:00:00Z+TO+' + Year + '-' + Month + '-31T00:00:00Z]');
         WP_Cal_get_posts_by_month( Month, Year );
         monthFollowLink(Month, Year);
     });
@@ -184,12 +184,12 @@ jQuery(document).ready(function(){
 
     WP_Cal_get_posts_by_month(c_month, c_year);
 
-    jQuery('.WP-Cal-popup').live({
+    $('.WP-Cal-popup').live({
         mouseenter: function() {
-            jQuery(this).find('div').show();
+            $(this).find('div').css( "display", "inline-table" );
         },
         mouseleave: function() {
-            jQuery(this).find('div').hide();
+            $(this).find('div').hide();
         }
     });
 
